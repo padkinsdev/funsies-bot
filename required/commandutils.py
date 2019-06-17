12a35972031d4dea849163cf7e64fb6f53d31c98
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-06-16 20:11:13 -0400

    Shortened and simplified commandutils.py

--- required/commandutils.py
+++ required/commandutils.py
@@ -1,39 +1,29 @@
-import os
-import sys
-import importlib
+import userHandler
+import permissions
 
-class Command:
-  def __init__(self, function, name="Unnamed", description="No description"):
-    self.name = name
-    self.description = description
-    self.__func = function
-  def __call__(self, *args, **kwargs):
-    self.__func(*args, **kwargs)
-
-class CommandParser:
+class Commandler:
   def __init__(self):
-    self.commandList = {}
-  def selectCommandByName(self, name):
-    if name in self.commandList.keys():
-      return self.commandList[name]
-    else:
-      return None
-  def setCommandLibPath(self, filePath):
-    os.environ['CMD_LIB'] = filePath
-  def liftCommands(self):
-    """
-    Generator function that returns the bot commands in a python file
-    """
-    path = os.environ.get("CMD_LIB")
-    modNameComponents = path.split("/")
-    modName = modNameComponents[len(modNameComponents)-1]
-    importlib.import_module(modName, __name__)
-    for item in sys.modules[modName]:
-      if callable(item):
-        yield Command(item, name=item.__name__)
-  def appendCommandsToList(self):
-    """
-    Uses self.listCommands to create a list of usable commands
-    """
-    for command in self.liftCommands():
-      self.commandList.update({command.name: command})
+    self.userDB = userHandler.UserDatabase()
+    self.perms = permissions.PermissionInteger()
+  def requiresPermission(self, permList):
+    def decorator(func):
+      def decorated(message):
+        userPerms = self.userDB.retrieveUserPerms(message.author.id)
+        if userPerms < 0:
+          self.userDB.insertNewUser(message.author.id, 1, str(message.author.name))
+        truePerms = self.perms.getPermInteger(permList)
+        if (userPerms & truePerms) == truePerms:
+          return func(message)
+        else:
+          return None
+      return decorated
+    return decorator
+  def serverSpecific(self, enabledServers):
+    def decorator(func):
+      def decorated(message):
+        if message.guild.id in enabledServers:
+          return func(message)
+        else:
+          return None
+      return decorated
+    return decorator
