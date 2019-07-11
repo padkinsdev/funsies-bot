05ea4f7eabd2b9d52f63c35810afa167b6a7cb4f
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-11 01:18:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ prefix = bd.prefix
 
 @client.event
 async def on_message(message):
-  if client.user.mention in message.mentions:
+  if client.user.mentioned_in(message):
     await message.channel.send("My prefix is " + prefix + " " + message.author.mention)
   if message.content.startswith(prefix):
     if " " in message.content:
