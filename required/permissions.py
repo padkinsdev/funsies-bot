f6e56abdb2ff956f5857e204f80f5cd69d3e6c3a
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-10 22:50:43 -0400

    Add files via upload

---
+++ required/permissions.py
@@ -0,0 +1,9 @@
+class PermissionInteger:
+  def __init__(self):
+    self.permlist = {"not_muted": 1, "no_cooldown": 2, "trusted": 4, "premium": 8, "owner": 16, "administrator": 32}
+  def getPermInteger(self, permList):
+    finalInt = 0
+    for permission in permList:
+      if permission in self.permlist.keys():
+        finalInt += self.permlist[permission]
+    return finalInt
