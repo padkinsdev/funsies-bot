00a23bee631b4789ac81c5a6f3f8ad03412e2de4
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-18 10:32:19 -0400

    How great you are, Sovereign LORD!
    How great you are, Sovereign LORD! There is no one like you, and there is no God but you, as we have heard with our own ears.

--- required/some_utils.py
+++ required/some_utils.py
@@ -1,4 +1,7 @@
 import discord
+from botfiles.bot_data import default_embed_color
+import datetime
 
-def create_embed(title, content, description=Empty, author=Empty, type="rich"):
-  pass
+def create_embed(title, content, color=default_embed_color, description=discord.Embed.Empty, author=discord.Embed.Empty, emb_type="rich", time_stamp=datetime.now(), url=discord.Embed.Empty):
+  embed = discord.Embed(title=title, content=content, color=color, description=description, author=author, type=emb_type, timestamp=time_stamp, url=url)
+  return embed
