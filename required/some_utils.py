45fcab381dc55acb66bcdbc49d2e638f497c7f0d
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-18 20:53:54 -0400

    Fixed syntax error

--- required/some_utils.py
+++ required/some_utils.py
@@ -3,6 +3,6 @@ from botfiles.bot_data import default_embed_color
 from botfiles.bot_data import client
 import datetime
 
-def create_embed(title, content, color=default_embed_color, description=discord.Embed.Empty, author=author=client.user.name, emb_type="rich", time_stamp=datetime.now(), url=discord.Embed.Empty):
+def create_embed(title, content, color=default_embed_color, description=discord.Embed.Empty, author=client.user.name, emb_type="rich", time_stamp=datetime.now(), url=discord.Embed.Empty):
   embed = discord.Embed(title=title, content=content, color=color, description=description, author=author, type=emb_type, timestamp=time_stamp, url=url)
   return embed
