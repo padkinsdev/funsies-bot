c06345fd0fc3d0f874a91750a7165c13a733ad8b
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-31 21:16:06 -0400

    Update bot_data.py

--- botfiles/bot_data.py
+++ botfiles/bot_data.py
@@ -1,5 +1,6 @@
 import discord
 import required.commandutils as cu
+import os
 
 client = discord.Client()
 gatekeeper = cu.Commandler()
@@ -7,3 +8,5 @@ gatekeeper = cu.Commandler()
 prefix = "f."
 
 default_embed_color = "#f57542"
+
+bucket_name = os.environ["AWS_BUCKET_NAME"]
