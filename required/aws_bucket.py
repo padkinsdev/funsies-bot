157261a2260fb749947c91823cebfaaf916125a7
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-07-31 22:55:40 -0400

    Update aws_bucket.py

--- required/aws_bucket.py
+++ required/aws_bucket.py
@@ -3,17 +3,17 @@ import boto3
 class AWSBucketManager:
   def __init__(self, bucket_name):
     self.client = boto3.client("s3")
-    self.bucket = boto3.Bucket("bucket_name")
+    #self.bucket = boto3.Bucket(bucket_name)
   def get_as_file(self, fname):
     try:
       with open(fname, "wb") as item:
-        self.bucket.download_fileobj(fname, item)
+        self.client.download_fileobj(fname, item)
       return True
     except:
       return False
   def upload_file(self, fpath, fname):
     try:
       with open(fpath, "rb") as item:
-        self.bucket.upload_fileobj(item, fname)
+        self.client.upload_fileobj(item, fname)
     except:
       return False
