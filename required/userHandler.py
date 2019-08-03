3442a1a2005137d2920365e089ceeea3a3a48b47
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-03 19:46:45 -0400

    Update userHandler.py

--- required/userHandler.py
+++ required/userHandler.py
@@ -1,56 +1,22 @@
-import sqlite3
-from os.path import isfile
-import required.aws_bucket
+import json
 
 class UserDatabase:
   def __init__(self):
-    self.__path = "userDB.db"
-    if isfile(self.__path):
-      pass
+    self.data = {}
+  def parse_to_dict(self, fobj):
+    """
+    Extracts the data from a *raw* JSON file object into a Python dictionary
+    """
+    self.data = json.load(fobj)
+    fobj.close()
+  def get_field(self, field_name):
+    if field_name in self.data.keys():
+      return self.data[field_name]
     else:
-      self.createDataBase()
-  def createDataBase(self):
-    connection, crsr = self.connectToDB()
-    init_one = "CREATE TABLE users (snowflake_id INTEGER PRIMARY KEY, permissions VARCHAR(5), balance INTEGER)"
-    crsr.execute(init_one)
-    connection.commit()
-    connection.close()
-  def connectToDB(self):
-    connection = sqlite3.connect(self.__path)
-    crsr = connection.cursor()
-    return connection, crsr
-  def insertNewUser(self, userId, permissionInt):
-    connection, crsr = self.connectToDB()
-    crsr.execute("INSERT INTO users VALUES (" + str(userId) + ", \"" + str(permissionInt) + "\", \"0\");")
-    connection.commit()
-    connection.close()
-  def modifyEntry(self, userId, newPermissions=None, newBal=None):
-    connection, crsr = self.connectToDB()
-    if newPermissions:
-      crsr.execute("UPDATE users SET permissions = \"" + str(newPermissions) + "\" WHERE snowflake_id = " + str(userId))
-    if newBal:
-      crsr.execute("UPDATE users SET balance = \"" + str(newBal) + "\" WHERE snowflake_id = " + str(userId))
-    connection.commit()
-    connection.close()
-  def retrieveUserPerms(self, userId):
-    connection, crsr = self.connectToDB()
-    crsr.execute("SELECT permissions FROM users WHERE snowflake_id = " + str(userId))
-    result = crsr.fetchone()
-    if result:
-      return result[0]
+      return None
+  def write_field(self, field_name, value):
+    if field_name in self.data.keys():
+      self.data[field_name] = value
+      return True
     else:
-      self.insertNewUser(str(userId), "1")
-      return "1"
-  def retrieveUserBal(self, userId):
-    connection, crsr = self.connectToDB()
-    crsr.execute("SELECT balance FROM users WHERE snowflake_id = " + str(userId))
-    result = crsr.fetchone()
-    if result:
-      return result[0]
-    else:
-      self.insertNewUser(str(userId), "0")
-      return "0"
-  def add_to_bal(self, user_id, amount):
-    balance = int(self.retrieveUserBal(user_id))
-    balance += amount
-    self.modifyEntry(user_id, newBal=balance)
+      return False
