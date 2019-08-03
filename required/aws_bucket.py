4c13115487d4db55938f61e3c8d0ca62406e9518
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-03 19:34:31 -0400

    Pass raw file obj in and out
    Heroku doesn't seem to allow writing to the file system

--- required/aws_bucket.py
+++ required/aws_bucket.py
@@ -7,15 +7,14 @@ class AWSBucketManager:
     self.bucket = self.s3.Bucket(bucket_name)
   def get_as_file(self, fname):
     try:
-      with open(fname, "wb") as item:
+      with open(fname, 'wb') as item:
         self.bucket.download_fileobj(fname, item)
-      return True, ""
+        return True, item
     except Exception as err:
-      return False, err
-  def upload_file(self, fpath, fname):
+      return None, err
+  def upload_file(self, fobj, fname):
     try:
-      with open(fpath, "rb") as item:
-        self.bucket.upload_fileobj(item, fname)
+      self.bucket.upload_fileobj(fobj, fname)
       return True, ""
     except Exception as err:
       return False, err
