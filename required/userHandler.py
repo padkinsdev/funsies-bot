9c27dc0705d992379059e371ad486f7e5b01b544
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-05 13:12:31 -0400

    Update userHandler.py

--- required/userHandler.py
+++ required/userHandler.py
@@ -29,7 +29,7 @@ class UserDatabase:
       self.data.update({user_id: {field_name: value}})
     return False
   def package_as_fobj(self, db_name="userDB.json"):
-    with open(db_name, 'w') as datafile:
+    with open(db_name, 'wb') as datafile:
       return datafile
   def add_new_user(self, user_id, field_dict={}):
     if user_id not in self.data.keys():
