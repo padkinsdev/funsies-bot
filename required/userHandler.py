e346b4fea512213ebce870e703da49681749992d
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-05 23:47:47 -0400

    Fixed type conversions

--- required/userHandler.py
+++ required/userHandler.py
@@ -11,23 +11,23 @@ class UserDatabase:
     fobj.close()
     print(self.data)
   def get_field(self, user_id, field_name):
-    if user_id in self.data.keys():
+    if str(user_id) in self.data.keys():
       if field_name in self.data[user_id].keys():
-        return self.data[user_id][field_name]
+        return self.data[str(user_id)][str(field_name)]
       else:
         return None
     else:
       self.add_new_user(user_id)
       return None
   def write_field(self, user_id, field_name, value):
-    if user_id in self.data.keys():
-      if field_name in self.data[user_id].keys():
-        self.data[user_id][field_name] = value
+    if str(user_id) in self.data.keys():
+      if str(field_name) in self.data[user_id].keys():
+        self.data[str(user_id)][str(field_name)] = str(value)
         return True
       else:
-        self.data[user_id].update({field_name: value})
+        self.data[user_id].update({str(field_name): str(value)})
     else:
-      self.data.update({user_id: {field_name: value}})
+      self.data.update({str(user_id): {str(field_name): str(value)}})
     return False
   def package_as_fobj(self, db_name="userDB.json"):
     with open(db_name, 'w') as dfile:
@@ -35,5 +35,5 @@ class UserDatabase:
     datafile = open(db_name, 'rb')
     return datafile
   def add_new_user(self, user_id, field_dict={}):
-    if user_id not in self.data.keys():
-      self.data.update({user_id: field_dict})
+    if str(user_id) not in self.data.keys():
+      self.data.update({str(user_id): field_dict})
