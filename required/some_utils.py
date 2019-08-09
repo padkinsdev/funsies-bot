7560a1b55dadb9dac3c018191afee0d41b7dae1c
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-09 14:28:59 -0400

    Delete some_utils.py

--- required/some_utils.py
+++
@@ -1,8 +0,0 @@
-import discord
-from botfiles.bot_data import default_embed_color
-from botfiles.bot_data import client
-import datetime
-
-def create_embed(title, content, color=default_embed_color, description=discord.Embed.Empty, author="Funsies", emb_type="rich", time_stamp=datetime.datetime.now(), url=discord.Embed.Empty):
-  embed = discord.Embed(title=title, content=content, color=color, description=description, author=author, type=emb_type, timestamp=time_stamp, url=url)
-  return embed
