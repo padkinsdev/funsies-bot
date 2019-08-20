4b471f56d1da889ef1d14d104772c582eb5022ba
Paul Adkins <50161615+padkinsdev@users.noreply.github.com>
2019-08-20 07:12:22 -0400

    Update preprocess.py

--- botfiles/preprocess.py
+++ botfiles/preprocess.py
@@ -22,11 +22,17 @@ async def check_afk(message):
     if afkstat:
       await message.channel.send(message.author.mention + " " + member.display_name + " is afk:\n " + afkstat)
 
+async def remove_afk(message):
+  if gatekeeper.userDB.get_field(message.author.id, "afk"):
+    gatekeeper.userDB.delete_field(message.author.id, "afk")
+    await message.channel.send(message.author.display_name + ", I removed your afk")
+
 ### Bullet list of things the bot needs to do before checking the command list
 async def preprocess_list(message):
   update_activity(message)
   await check_level_up(message)
   await check_afk(message)
+  await remove_afk(message)
 
 ### Utilities
 def uptick(user_id, field_name):
